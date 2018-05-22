ed5a2b962b68d5826a1dcd0fa1eb7b625cc24008
Elijah Zakirov <jangolle@mail.ru>
2018-05-22 08:11:18 +0300

    Initial commit

---
+++ youtube-search-app/src/components/VideoList.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import VideoListItem from './VideoListItem';
+
+class VideoList extends Component {
+  render() {
+    return (
+        <ul className="col-md-4 list-group">
+          {this.props.videos.map((video) => {
+            return <VideoListItem onVideoSelect={this.props.onVideoSelect} key={video.etag} video={video}/>;
+          })}
+        </ul>
+    );
+  }
+}
+
+export default VideoList;
